57647f09c61f5234c35f7d576c64961e2aee2579
Graham Lee <ghmlee@ghmlee.com>
2015-04-20 18:54:43 +0900

    A thread is detached to get stats for each container.

--- src/curiosity.rs
+++ src/curiosity.rs
@@ -1,9 +1,12 @@
 use std::thread;
-use std::sync::{Arc, RwLock};
-use std::sync::mpsc::{self, Receiver};
+use std::sync::mpsc::{self, Sender, Receiver};
+use std::collections::HashMap;
+use std::collections::hash_map::Entry::{Occupied, Vacant};
 use time;
 use cosmos::Cosmos;
 use container;
+use container::Container;
+use rustc_serialize::json;
 
 pub struct Curiosity;
 
@@ -16,46 +19,99 @@ impl Curiosity {
         let host = host.to_string();
         let planet_name = planet_name.to_string();
         
-        let mut last_timestamp = time::precise_time_s() as u64;
-        let lock = Arc::new(RwLock::new(String::new()));
-        let lock_copy = lock.clone();
-        
-        let (tx, rx) = mpsc::channel();
-        thread::spawn(move|| { Curiosity::get_containers(&lock); });
-        thread::spawn(move|| { Curiosity::post_containers(&rx, &host, &planet_name); });
+        let mut timestamp = time::precise_time_s() as u64;
+        let mut map: HashMap<String, Container> = HashMap::new();
+
+        let (container_tx, container_rx) = mpsc::channel();
+        let (cosmos_tx, cosmos_rx) = mpsc::channel();
+        thread::spawn(move|| { Curiosity::get_containers(&container_tx); });
+        thread::spawn(move|| { Curiosity::post_containers(&cosmos_rx, &host, &planet_name); });
 
         loop {
-            let current_timestamp = time::precise_time_s() as u64;
-            let diff = current_timestamp - last_timestamp;
-            if diff < interval { thread::sleep_ms(1 * 1000); continue; }
-            last_timestamp = current_timestamp;
-            
-            thread::sleep_ms(1);
-            let val = match lock_copy.read() {
-                Ok(val) => val,
-                Err(_) => { continue; }
+            let containers = match container_rx.try_recv() {
+                Ok(containers) => containers,
+                Err(_) => { thread::sleep_ms(100); continue; }
             };
 
-            match tx.send(val.clone()) {
+            for container in containers.iter() {
+                let value = match map.entry(container.Id.clone()) {
+                    Occupied(mut entry) => { entry.get_mut().clone() }
+                    Vacant(entry) => { entry.insert(container.clone()); continue; }
+                };
+                let max = value.Stats.Cpu.TotalUtilization;
+                let current = container.Stats.Cpu.TotalUtilization;
+                if current - max > 0.0 {
+                    map.remove(&container.Id);
+                    map.insert(container.Id.clone(), container.clone());
+                }
+            }
+
+            let now = time::precise_time_s() as u64;
+            let diff = now - timestamp;
+            if diff < interval { continue; }
+            timestamp = now;
+
+            let mut cosmos_containers: Vec<Container> = Vec::new();
+            for x in map.values() { cosmos_containers.push(x.clone()); }
+
+            let body = match json::encode(&cosmos_containers) {
+                Ok(encoded) => encoded,
+                Err(e) => { println!("{}", e); continue; }
+            };
+
+            match cosmos_tx.send(body) {
                 Ok(_) => {}
                 Err(e) => { println!("{}", e); continue; }
             }
+
+            map.clear();
         }
     }
     
-    fn get_containers(lock: &RwLock<String>) {
+    fn get_containers(tx: &Sender<Vec<Container>>) {
+        let tx = tx.clone();
         loop {
-            let containers = match container::get_containers_as_str() {
+            let containers = match container::get_containers() {
                 Ok(containers) => containers,
                 Err(e) => { println!("{}", e); continue; }
             };
 
-            thread::sleep_ms(1);
-            let mut val = match lock.write() {
-                Ok(val) => val,
-                Err(_) => { continue; }
-            };
-            *val = containers;
+            let (stats_tx, stats_rx) = mpsc::channel();
+            let mut index: u64 = 0;
+            for container in containers.iter() {
+                if container.Status.contains("Up") == false { continue; }
+
+                let container = container.clone();
+                let stats_tx = stats_tx.clone();
+                thread::spawn(move|| {
+                    let result = container::get_stats_as_cosmos_container(&container);
+                    match stats_tx.send(result) {
+                        Ok(_) => {}
+                        Err(e) => { println!("{}", e); return; }
+                    }
+                });
+                index += 1;
+            }
+
+            let mut cosmos_containers: Vec<Container> = Vec::new();
+            for _ in 0..index {
+                let result = match stats_rx.recv() {
+                    Ok(result) => result,
+                    Err(e) => { println!("{}", e); continue; }
+                };
+
+                let cosmos_container = match result {
+                    Ok(container) => container,
+                    Err(e) => { println!("{}", e); continue; }
+                };
+
+                cosmos_containers.push(cosmos_container);
+            }
+
+            match tx.send(cosmos_containers) {
+                Ok(_) => {}
+                Err(e) => { println!("{}", e); continue; }
+            }
         }
     }
 
